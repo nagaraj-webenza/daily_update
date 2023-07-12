fd2286d23d6a8999c56df86df1dd80aa152c6599
Nagaraj Sajjan <63628911+nagaraj-webenza@users.noreply.github.com>
2023-07-12 11:55:22 +0530

    Create daily_update.sh

---
+++ daily_update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
+sudo apt-get -y clean
+sudo apt-get -y remove
